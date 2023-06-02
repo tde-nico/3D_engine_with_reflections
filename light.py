e1a10b791f1a6e849e23446b3010850f250b91b0
tde-nico <101901057+tde-nico@users.noreply.github.com>
2023-06-02 10:13:49 +0200

    init

---
+++ light.py
@@ -0,0 +1,18 @@
+import glm
+
+
+class Light:
+	def __init__(self, position=(50, 50, -10), color=(1, 1, 1)):
+		self.position = glm.vec3(position)
+		self.color = glm.vec3(color)
+		self.direction = glm.vec3(0, 0, 0)
+		# intensities
+		self.Ia = 0.1 * self.color
+		self.Id = 0.8 * self.color
+		self.Is = 1.0 * self.color
+		# view matrix
+		self.m_view_light = self.get_view_matrix()
+
+	def get_view_matrix(self):
+		return glm.lookAt(self.position, self.direction, glm.vec3(0, 1, 0))
+
